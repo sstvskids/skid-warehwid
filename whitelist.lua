fc5295291577882c772f9fad7b54ffb5a7e4e816
sstvskids <126665568+sstvskids@users.noreply.github.com>
2023-04-29 20:39:43 +1000

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -2,4 +2,5 @@ return {
     "2B749D88-F93C-460B-BB7E-D528289771C5", -- stav
     "98FCAEA8-744B-468C-A25F-CA35EFD50242", -- kryptonite owner skxdded
     "D66993E2-9D51-46E2-B0BA-F48F61C34416", -- drdweebq
+    "B34099C7-A847-4CCE-8900-1297B698F274", -- deadlydeath
 }
