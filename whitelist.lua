6155991647c06a8bbd93525759b83fba8ccef010
sstvskids <126665568+sstvskids@users.noreply.github.com>
2023-04-30 08:27:31 +1000

    Update whitelist.lua
    Added someone

--- whitelist.lua
+++ whitelist.lua
@@ -1,5 +1,4 @@
 -- Whitelist for the newest private Skid-Ware
-
 return {
     "2B749D88-F93C-460B-BB7E-D528289771C5", -- stav
     "98FCAEA8-744B-468C-A25F-CA35EFD50242", -- kryptonite owner skxdded
@@ -7,4 +6,5 @@ return {
     "B34099C7-A847-4CCE-8900-1297B698F274", -- deadlydeath
     "A518B6F7-B061-4D26-8206-6B6D216D616B", -- kizo
     "E3B89714-2BC7-4BA5-99A5-4373E3A53E09", -- destinity
+    "37F019DF-56C1-4777-834C-71BE37B55BDC", -- xenzo
 }
